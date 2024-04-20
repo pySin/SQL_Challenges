e83c1779d892fbd273069a269465db9335897be2
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-04-20 03:05:27 +0100

    Delete challenges/select-basic directory

--- challenges/select-basic/cities_in_us.sql
+++
@@ -1,6 +0,0 @@
-# Query all columns for all American cities in the CITY table with populations larger than 100000. 
-# The CountryCode for America is USA.
-
-SELECT * FROM world.city
-WHERE COuntryCode  = "USA"
-AND Population > 100000;
