10d2ef14963665ff0ab449261ee2c41ce60b0d85
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-04-23 18:10:55 +0100

    Add files via upload

---
+++ challenges/select-basic/double-bar-comparison/project/sql_result/base_sql_query.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class BaseQuery(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def query(self):
+        pass
+        query_instructions = """
+        """
+
+        return query_instructions
